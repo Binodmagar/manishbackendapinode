649ff9fe4e7c3a87a68d6d838bf50923766f16ab
Binodmagar <ranabinod615@gmail.com>
2020-01-29 09:19:41 +0545

    [#9] basic changes in expense model

--- models/expense.js
+++ models/expense.js
@@ -18,7 +18,7 @@ const expenseSchema = new mongoose.Schema({
         required: true
     },
     date:{
-        type: Date,
+        type: String,
         required: true
     },
     description:{
